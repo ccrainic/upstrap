626463413d5a5ff8771d22f3a7449cb1132bc8b3
muschellij2 <muschellij2@gmail.com>
2018-08-20 12:16:37 -0400

    updated man

--- R/upstrap.R
+++ R/upstrap.R
@@ -1,13 +1,18 @@
-#' Title
+#' Upstrap Resampling
 #'
-#' @param data
-#' @param statistic
-#' @param R
-#' @param n.grid.multiplier number of grid points for the multiplier of the sample size
+#' @param data The data as a matrix or data frame.
+#' If it is a matrix or data frame then each row
+#' is considered as one multivariate observation.
+#' @param statistic A function which when applied to data returns a vector
+#' containing the statistic(s) of interest.
+#' @param R The number of upstrap replicates for each point on the grid of
+#' range multipliers. Usually this will be a single positive integer.
+#' @param n.grid.multiplier number of grid points for the
+#' multiplier of the sample size.
 #' @param range_multiplier minimum and maximum multiplier for the sample size
-#' @param ...
+#' @param ... additional arguments to pass to \code{statistic}.
 #'
-#' @return
+#' @return An object
 #' @export
 #'
 #' @examples
@@ -19,16 +24,17 @@
 #' data$MtS_SA = data$rdi4p>=15
 #' statistic = function(data) {
 #' fit<-glm(MtS_SA ~ gender + age_s1 + bmi_s1 +
-#' HTNDerv_s1 + age_s1*HTNDerv_s1, family="binomial", data=temp_data)
-#' coef(summary(fit))[,4][5]<0.05
+#' HTNDerv_s1 + age_s1*HTNDerv_s1, family="binomial", data=data)
+#' # could use broom
+#' #  tid = broom::tidy(fit)
+#' #  pval = tid$p.value[ tid$term == "HTNDerv_s1"]
+#' smod = coef(summary(fit))
+#' pval = smod["HTNDerv_s1", "Pr(>|z|)"]
+#' pval < 0.05
 #' }
-
-}
-#' data =
-#' statistic =
-
-# fit the same model on the upstrapped data
-
+#' res = upstrap(data, statistic = statistic, R = 100, n.grid.multiplier = 5,
+#' range_multiplier = c(1, 3))
+#' res = colMeans(res)
 upstrap = function(data, statistic, R = 10000, n.grid.multiplier = 21,
                    range_multiplier = c(1, 5), ...) {
 
@@ -62,8 +68,8 @@ upstrap = function(data, statistic, R = 10000, n.grid.multiplier = 21,
   # set the grid of multipliers for the original sample size
   # here 1.2 stands for a sampel size that is 20% larger than the original sample size
   multiplier.grid = seq(from = min.multiplier,
-                      to = max.multiplier,
-                      length=n.grid.multiplier)
+                        to = max.multiplier,
+                        length = n.grid.multiplier)
 
   # set the number of upstraps for each grid point;
   # this impacts running time and variability of the sample size calculation:
@@ -74,12 +80,15 @@ upstrap = function(data, statistic, R = 10000, n.grid.multiplier = 21,
 
   # build the matrix that will contain whether or not the null hypothesis that the
   # HTN is zero for a given multiplier (r) and upstrap sample (u)
-  check<-matrix(rep(NA,n.upstrap*n.grid.multiplier),
-                ncol=n.grid.multiplier)
+  check <- matrix(
+    nrow = n.upstrap,
+    ncol = n.grid.multiplier)
 
+  colnames(check) = round(n.oss*multiplier.grid)
   # here j is the index of the multiplier of the original sample size, n.oss
   for (j in 1:n.grid.multiplier)
   {#Each loop corresponds to an increase in the sample size
+    N =  round(n.oss*multiplier.grid[j])
 
     # here u is the u-th upstrap for the r-th sample size multiplier
     # this simulation can/will be done more efficiently using matrices
@@ -90,16 +99,17 @@ upstrap = function(data, statistic, R = 10000, n.grid.multiplier = 21,
       # from 1, ..., n.oss (original sample size)
       # size equal to original sample size times the multiplier
       # sampling is with replacement
-      temp_index<-sample(1:n.oss,
-                         round(n.oss*multiplier.grid[j]),
-                         replace=TRUE)
+      temp_index <- sample.int(
+        n.oss, size = N,
+        replace = TRUE)
 
       # extract the data (covariates and outcome) using the upstrap sample index
-      temp_data<-dat[temp_index,]
+      temp_data <- data[temp_index,]
 
 
       # obtain the p-value for HTN in the upstrapped data
-      check[u,j]<-coef(summary(fit))[,4][5]<0.05
+      check[u,j] <- statistic(temp_data, ...)
     }
   }
+  return(check)
 }
